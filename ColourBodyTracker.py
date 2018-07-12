0ce9fabdae560b6e72ff58c7d3121cd7c762f1b1
Mike Vanis <hello@mikevanis.com>
2018-07-12 23:48:36 +0100

    now with video test, hsv values tweaked

--- ColourBodyTracker.py
+++ ColourBodyTracker.py
@@ -1,41 +1,67 @@
 import cv2
 import imutils
 import sys
+import argparse
 sys.path.insert(0, 'CameraController')
 from CameraController import CameraController
 from ShapeDetector import ShapeDetector
 from MarkerFinder import MarkerFinder
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--test_video")
+args = parser.parse_args()
+
 sd = ShapeDetector()
-marker_finder = MarkerFinder()
+marker_finder = MarkerFinder(show_mask=True)
 
 if __name__ == '__main__':
-    cam = CameraController(width=600, height=400)
-    cam.start()
+    if args.test_video is None:
+        cam = CameraController(width=600, height=400)
+        cam.start()
 
-    try:
-        while True:
-            current_frame = cam.get_image()
+        try:
+            while True:
+                current_frame = cam.get_image()
 
-            if current_frame is not None:
-                points = marker_finder.find(current_frame)
+                if current_frame is not None:
+                    points = marker_finder.find(current_frame)
 
-                for p in points:
-                    cv2.circle(current_frame, p, 5, (255, 0, 255), -1)
+                    for p in points:
+                        cv2.circle(current_frame, p, 5, (255, 0, 255), -1)
 
-                cv2.imshow("Result", current_frame)
+                    cv2.imshow("Result", current_frame)
 
+                key = cv2.waitKey(1)
+
+
+                if key == ord('q'):
+                    cam.stop()
+                    cam.join()
+                    cv2.destroyAllWindows()
+                    break
+
+        except KeyboardInterrupt:
+            cam.stop()
+            cam.join()
+            cv2.destroyAllWindows()
+            pass
+
+    else:
+        cap = cv2.VideoCapture(args.test_video)
+        while(cap.isOpened()):
+            ret, current_frame = cap.read()
+            current_frame = imutils.resize(current_frame, 600)
+            current_frame = imutils.rotate_bound(current_frame, 270)
+
+            points = marker_finder.find(current_frame)
+
+            for p in points:
+                cv2.circle(current_frame, p, 5, (255, 0, 255), -1)
+
+            cv2.imshow("Result", current_frame)
             key = cv2.waitKey(1)
 
-
             if key == ord('q'):
-                cam.stop()
-                cam.join()
+                cap.release()
                 cv2.destroyAllWindows()
                 break
-
-    except KeyboardInterrupt:
-        cam.stop()
-        cam.join()
-        cv2.destroyAllWindows()
-        pass
--- MarkerFinder.py
+++ MarkerFinder.py
@@ -1,11 +1,12 @@
 import cv2
-
+import numpy as np
 
 class MarkerFinder:
 
     # Constructor
-    def __init__(self, min_radius=10):
+    def __init__(self, min_radius=10, show_mask="False"):
         self.min_radius = min_radius
+        self.show_mask = show_mask
         pass
 
     # Find markers and return point list
@@ -13,11 +14,19 @@ class MarkerFinder:
         points = list()
 
         blurred = cv2.GaussianBlur(image, (11, 11), 0)
-        blurred = (255 - blurred)
+        #blurred = (255 - blurred)
         frame_to_thresh = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        lower_cyan = (85 - 10, 70, 50)
-        upper_cyan = (85 + 10, 255, 255)
-        thresh = cv2.inRange(frame_to_thresh, lower_cyan, upper_cyan)
+        #blue_lower = np.array([100, 150, 20], np.uint8)
+        #blue_upper = np.array([140, 255, 255], np.uint8)
+        #thresh = cv2.inRange(frame_to_thresh, blue_lower, blue_upper)
+        #lower_cyan = (90 - 10, 70, 50)
+        #upper_cyan = (90 + 10, 255, 255)
+        #thresh = cv2.inRange(frame_to_thresh, lower_cyan, upper_cyan)
+        lower_red = (170, 150, 50)
+        upper_red = (180, 255, 255)
+
+        thresh = cv2.inRange(frame_to_thresh, lower_red, upper_red)
+
         thresh = cv2.erode(thresh, None, iterations=2)
         thresh = cv2.dilate(thresh, None, iterations=2)
 
@@ -29,6 +38,12 @@ class MarkerFinder:
             center = (int(moment["m10"] / moment["m00"]), int(moment["m01"] / moment["m00"]))
 
             if radius > self.min_radius:
+                print(radius)
                 points.append(center)
+
+        if self.show_mask is True:
+            thresh_output = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+            output = np.hstack((thresh_output, blurred))
+            cv2.imshow("Colour Tracking", output)
 
         return points
